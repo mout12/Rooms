76b3fa162d59ada69f58a8dc43ba1f115279f495
Jackson Buck <mout12@gmail.com>
2024-01-26 16:08:22 -0800

    Let Rooms connect to each other

--- Rooms/Room.swift
+++ Rooms/Room.swift
@@ -1,6 +1,15 @@
 class Room {
     let name: String
+    var connectedRooms: [Room]
+
     init(name: String) {
         self.name = name
+        self.connectedRooms = []
+    }
+
+    func Connect(rooms: Room ...) {
+        for room in rooms {
+            self.connectedRooms.append(room)
+        }
     }
 }
--- Rooms/main.swift
+++ Rooms/main.swift
@@ -1,3 +1,9 @@
 let livingRoom = Room(name: "Living Room")
+let garage = Room(name: "Garage")
+let bedroom = Room(name: "Bedroom")
 
-print("Hello " + livingRoom.name)
+livingRoom.Connect(rooms: garage, bedroom)
+
+for room in livingRoom.connectedRooms {
+    print(room.name)
+}
